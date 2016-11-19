55eac20e4ebb902b8e141bfb0deb935c1b09c858
Ivange Larry <ivangelarry@gmail.com>
2016-11-19 18:58:34 +0100

    Created project structure

---
+++ src/main/java/com/skylabase/AgroMarketPlaceApplication.java
@@ -0,0 +1,12 @@
+package com.skylabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroMarketPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroMarketPlaceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/skylabase/AgroMarketPlaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skylabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgroMarketPlaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
