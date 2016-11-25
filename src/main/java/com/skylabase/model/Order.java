a51e981796d8415e29455babba61722a7565157e
Glenn-Faison <glennfaison@gmail.com>
2016-11-25 21:33:40 +0100

    Structure of the 0rder endpoint.

---
+++ src/main/java/com/skylabase/model/Order.java
@@ -0,0 +1,54 @@
+package com.skylabase.model;
+
+import org.springframework.data.annotation.Id;
+import java.util.Date;
+
+public class Order {
+
+    @Id
+    String id;
+    String buyer_id;
+    String product_id;
+    Date date;
+    int quantity;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBuyer_id() {
+        return buyer_id;
+    }
+
+    public void setBuyer_id(String buyer_id) {
+        this.buyer_id = buyer_id;
+    }
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
