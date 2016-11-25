ad65645ee970c1df6ada784d9494de6b4df029a6
Ivange Larry <ivangelarry@gmail.com>
2016-11-25 15:49:20 +0100

    Removed unused import

--- src/main/java/com/skylabase/rest/controllers/CategoryRestController.java
+++ src/main/java/com/skylabase/rest/controllers/CategoryRestController.java
@@ -1,14 +1,19 @@
 package com.skylabase.rest.controllers;
 
-import com.skylabase.model.Category;
-import com.skylabase.service.CategoryService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.skylabase.model.Category;
+import com.skylabase.service.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
