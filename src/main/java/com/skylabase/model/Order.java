314d0b263ad11327dd168b3c9eacd2599f561089
Ivange Larry <ivangelarry@gmail.com>
2016-12-21 21:05:29 +0100

    Removed an unused import

--- src/main/java/com/skylabase/model/Order.java
+++ src/main/java/com/skylabase/model/Order.java
@@ -8,7 +8,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.List;
 
