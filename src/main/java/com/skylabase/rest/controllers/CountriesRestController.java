63e50d4f2541a3337eab4cd81ebeaf621a8ce785
Glenn-Faison <glennfaison@gmail.com>
2016-12-24 01:58:30 +0100

    Edited Country Rest controller and renamed it.

--- src/main/java/com/skylabase/rest/controllers/CountriesRestController.java
+++ src/main/java/com/skylabase/rest/controllers/CountriesRestController.java
@@ -5,14 +5,12 @@ import com.skylabase.model.Country;
 import com.skylabase.service.CountryService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.naming.OperationNotSupportedException;
@@ -20,38 +18,39 @@ import java.util.List;
 
 @RestController
 @Api("Country")
-@RequestMapping(CountryRestController.COUNTRY_REQUEST_MAPPING)
-public class CountryRestController {
+@RequestMapping(CountriesRestController.COUNTRY_REQUEST_MAPPING)
+public class CountriesRestController {
 
     @Autowired
     private CountryService countryService;
 
     public static final String COUNTRY_REQUEST_MAPPING = "/api/v1/countries";
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    public ResponseEntity<Country> createCountry(@RequestBody Country country) {
+        final Country savedCountry = countryService.create(country);
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ServletUriComponentsBuilder.fromCurrentRequest().path("/").buildAndExpand("").toUri());
+        return new ResponseEntity<>(savedCountry, headers, HttpStatus.CREATED);
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Country>> getAllCountries() {
-        final List<Country> countries = countryService.findAll();
-        return new ResponseEntity<List<Country>>(countries, HttpStatus.OK);
+    public ResponseEntity<Page<Country>> getAllCountries(@RequestParam(value = "name", defaultValue = "", required = false) String name,
+                                                         @RequestParam(value = "pageSize", defaultValue = "10") String pageSize,
+                                                         @RequestParam(value = "pageNumber", defaultValue = "0") String pageNumber) {
+        int pgSize = Integer.valueOf(pageSize);
+        int pgNum = Integer.valueOf(pageNumber);
+        final Page<Country> countries = countryService.findAll(new PageRequest(pgNum, pgSize));
+        return new ResponseEntity<>(countries, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Country> getCountry(@PathVariable("id") Long id) {
         final Country country = countryService.findById(id);
         if (country == null) {
-            return new ResponseEntity<Country>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<Country>(country, HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
-    public ResponseEntity<Country> createCountry(@RequestBody Country country) {
-        if (countryService.exists(country)) {
-            return new ResponseEntity<Country>(HttpStatus.CONFLICT);
-        }
-        final Country savedCountry = countryService.create(country);
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ServletUriComponentsBuilder.fromCurrentRequest().path("/").buildAndExpand("").toUri());
-        return new ResponseEntity<Country>(savedCountry, headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(country, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
@@ -60,32 +59,32 @@ public class CountryRestController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Country> deleteCountry(@PathVariable("id") Long id) {
+    public ResponseEntity<Void> deleteCountry(@PathVariable("id") Long id) {
         final Country country = countryService.findById(id);
         if (country == null) {
-            return new ResponseEntity<Country>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        countryService.delete(country);
-        return new ResponseEntity<Country>(HttpStatus.OK);
+        countryService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping(value = "{countryId}/cities", method = RequestMethod.GET)
     public ResponseEntity<List<City>> getCities(@PathVariable("id") Long countryId) {
         final Country country = countryService.findById(countryId);
         if (country == null) {
-            return new ResponseEntity<List<City>>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<List<City>>(country.getCities(), HttpStatus.OK);
+        return new ResponseEntity<>(country.getCities(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "{countryId}/cities", method = RequestMethod.POST)
     public ResponseEntity<Country> addCity(@PathVariable("id") Long countryId, @RequestBody City city) {
         final Country country = countryService.findById(countryId);
         if (country == null) {
-            return new ResponseEntity<Country>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         country.addCity(city);
         countryService.update(country);
-        return new ResponseEntity<Country>(country, HttpStatus.OK);
+        return new ResponseEntity<>(country, HttpStatus.OK);
     }
 }
--- src/main/java/com/skylabase/service/CountryService.java
+++ src/main/java/com/skylabase/service/CountryService.java
@@ -1,6 +1,19 @@
 package com.skylabase.service;
 
 import com.skylabase.model.Country;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public interface CountryService extends GenericService<Country> {
+public interface CountryService {
+    Country create(Country instance);
+
+    Country findById(Long id);
+
+    Page<Country> findAll(Pageable pageable);
+
+    Country update(Country instance);
+
+    void delete(Long countryId);
+
+    boolean exists(Long countryId);
 }
