c26b3343fd6abc1442f2afb77835e14e42bf1cbd
Ivange Larry <ivangelarry@gmail.com>
2017-02-04 20:59:43 +0100

    product: Update product endpoint

--- src/main/java/com/skylabase/agromarketplace/service/ProductService.java
+++ src/main/java/com/skylabase/agromarketplace/service/ProductService.java
@@ -1,6 +1,10 @@
 package com.skylabase.agromarketplace.service;
 
 import com.skylabase.agromarketplace.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductService extends GenericService<Product> {
+
+    Page<Product> listAllByPage(Pageable pageable);
 }
